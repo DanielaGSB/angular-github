30c555aeb27dbd0bf945522c653ee523a8e84858
Daniela <daniela.galeote@gmail.com>
2016-01-27 15:41:04 +0000

    Adds autosearch on blur

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -2,13 +2,15 @@ githubUserSearch.controller('GitUserSearchController', ['$resource', function($r
   var self = this;
 
   var access_token = '1a90749a3de6d3f9d941e7e0b5a872574b8efdfe';
-
   var searchResource = $resource('https://api.github.com/search/users');
 
   self.doSearch = function() {
 
     self.searchResult = searchResource.get(
-      { q: self.searchTerm }
+      {
+        // access_token: access_token,
+        q: self.searchTerm,
+     }
     );
   };
 }]);
